4e091686f2cda504ae61b59d84dccde63329cbcd
mwahkeymer <audreymwakima@gmail.com>
2024-03-08 06:01:47 +0000

    pascal1

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,7 +8,7 @@ def pascal_triangle(n):
     """Create a function def pascal_triangle(n): that returns a list of lists
     of integers representing the Pascal’s triangle of n
     """
-    res = []
+    result = []
     if n > 0:
         for i in range(1, n + 1):
             level = []
@@ -16,5 +16,5 @@ def pascal_triangle(n):
             for j in range(1, i + 1):
                 level.append(C)
                 C = C * (i - j) // j
-            res.append(level)
-    return res
+            result.append(level)
+    return result
